3c62d5dffc6ccaf71a5b8a80240663483de04c0e
tsandee <tbsandee@orangebikelabs.com>
2021-08-10 16:55:51 -0500

    a few minor naming updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "cyclist_of_msn",
+      name: "cyclists_of_msn",
       script: "node_modules/.bin/ts-node",
       args: "src/server.ts",
       log_date_format: "YYYY-MM-DD HH:mm Z"
