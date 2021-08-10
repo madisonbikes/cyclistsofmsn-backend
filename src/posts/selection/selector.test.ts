69d9a575ccc170c9df37a09f6175e5778b5a4061
tsandee <tbsandee@orangebikelabs.com>
2021-08-10 14:29:08 -0500

    improve some testing

--- src/posts/selection/selector.test.ts
+++ src/posts/selection/selector.test.ts
@@ -1,6 +1,9 @@
-import { setupSuite, testContainer } from "../../test";
+import { NotVeryRandom, setupSuite, testContainer } from "../../test";
 import { Image, ImageDocument, PostHistory } from "../../database";
 import { PostSelector } from "./selector";
+import assert from "assert";
+import { startOfToday, subDays } from "date-fns";
+import { RandomProvider } from "../../utils";
 
 describe("test post image selector components", () => {
   setupSuite({ withDatabase: true });
@@ -14,26 +17,51 @@ describe("test post image selector components", () => {
   });
 
   describe("selector", () => {
-    it("start", async () => {
-      await createImage()
-
-      const selector = testContainer().resolve(PostSelector);
+    it("fail with no image", async () => {
+      const selector = buildSelector();
       const post = await selector.nextPost();
-
-      const post2 = await selector.nextPost();
-      expect(post.isOk()).toBeTruthy();
+      expect(post.isError()).toBeTruthy();
     });
 
-    it("start", async () => {
-      await createImage()
+    it("succeed with one image", async () => {
+      const image = await createImage();
 
-      const selector = testContainer().resolve(PostSelector);
+      const selector = buildSelector();
       const post = await selector.nextPost();
       expect(post.isOk()).toBeTruthy();
+      assert(post.isOk());
+      expect(post.value.id).toEqual(image.id);
+    });
+
+    it("pick unused image over a single used one", async () => {
+      const image = await createImage();
+      await createPost(image, subDays(startOfToday(), 1));
+
+      const newImage = await createImage("newImage");
+
+      const selector = buildSelector();
+      const post = await selector.nextPost();
+      expect(post.isOk()).toBeTruthy();
+      assert(post.isOk());
+      expect(post.value.id).toEqual(newImage.id);
+    });
+
+    it("pick seasonal repost over non-seasonal unposted", async () => {
+      const nonSeasonalImage = await createImage("nonSeasonalImage", subDays(startOfToday(), 60));
+
+      const seasonalImage = await createImage("seasonalImage");
+      await createPost(seasonalImage, subDays(startOfToday(), 190));
+
+      const selector = buildSelector();
+      const post = await selector.nextPost();
+      expect(post.isOk()).toBeTruthy();
+      assert(post.isOk());
+      expect(post.value.id).toEqual(seasonalImage.id);
     });
   });
 
-  async function createImage(name = "testImage", exif_createdon?: Date) {
+
+  async function createImage(name = "testImage", exif_createdon: Date | undefined = startOfToday()) {
     const image = new Image();
     image.filename = name;
     image.exif_createdon = exif_createdon;
@@ -45,5 +73,13 @@ describe("test post image selector components", () => {
     post.image = image;
     post.timestamp = postDate;
     return post.save();
+  }
+
+  /** build post selector that uses deterministic RNG so testing is reliable */
+  function buildSelector() {
+    return testContainer()
+      .createChildContainer()
+      .register<RandomProvider>(RandomProvider, { useValue: new NotVeryRandom(101) })
+      .resolve(PostSelector);
   }
 });
