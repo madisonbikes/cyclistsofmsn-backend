bce8afebbbae2e3eb8d61af940acc6cd1349ccf7
Ben Sandee <tbsandee@orangebikelabs.com>
2023-01-10 22:20:18 -0600

    refactor: use coerce instead of preprocess for optional number

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,14 +1,6 @@
 import { z } from "zod";
 
-const optionalNumberSchema = z.preprocess((arg) => {
-  if (arg === undefined) {
-    return undefined;
-  } else {
-    return parseInt(arg as string);
-  }
-}, z.number().optional());
-
 export const GetImageQuerySchema = z.object({
-  width: optionalNumberSchema,
-  height: optionalNumberSchema,
+  width: z.coerce.number().optional(),
+  height: z.coerce.number().optional(),
 });
