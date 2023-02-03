dca3a4d52373d5b6ecda923322a0d3ad465e9071
Ben Sandee <tbsandee@orangebikelabs.com>
2023-02-03 17:07:39 -0600

    test: exclude a couple files from coverage reports

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import "reflect-metadata";
 import { NowProvider, RandomProvider } from "../utils";
 import supertest from "supertest";
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import pino, { Logger, stdSerializers, TransportTargetOptions } from "pino";
 import { initEnv } from "./env";
 
